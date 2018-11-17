a7203f91c8f0ca52071b9a9f1bc115db8781d9c8
Matthew Honnibal <honnibal+gh@gmail.com>
2018-11-17 04:41:22 +0100

    Commit textcat experiment code

---
+++ lmao-imdb-1k/polyaxon_setup.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+# Install Python 3.6 
+apt-get update -y
+apt-get install -y wget curl python3.6 python3.6-venv python3.6-dev python3.6-distutils build-essential
+
+python3.6 -m venv env3.6
+source env3.6/bin/activate
+pip install -U pip
+pip install "spacy-nightly==2.1.0a2" polyaxon-helper
+python -m spacy download en_vectors_web_lg
---
+++ lmao-imdb-1k/run.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+env3.6/bin/python pretrain_textcat.py 128 1000 en_vectors_web_lg
